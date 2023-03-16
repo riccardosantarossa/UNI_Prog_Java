dc9a0cccebf0060002346ca78d53fba82f5a95bd
Riccardo Santarossa <brudo1968@libero.it>
2023-03-16 11:30:15 +0100

    FINITO TOTALE

--- ES1.java
+++ ES1.java
@@ -6,9 +6,9 @@ public class ES1
 
             String out = btrSucc("--++.-");
             System.out.println(out);
-
+        
         DEBUG DELLA PROCEDURA C1
-
+        
             String out = compl1("00111100");
             System.out.println(out);
         */
@@ -51,17 +51,18 @@ public class ES1
      public static String compl1( String num) 
      {
         String c1 = "";
-
-        for (int i = 0; i < num.length(); i++) 
+        if (num.equals(""))
+            return c1;
+        else 
         {
-            if (num.charAt(i) == '0')
-                c1 += "1";
-            else
-                c1+= "0";
+            for (int i = 0; i < num.length(); i++) 
+            {
+                if (num.charAt(i) == '0')
+                    c1 += "1";
+                else
+                    c1+= "0";
+            }
         }
-
         return c1;
      }
-
-    
 }
