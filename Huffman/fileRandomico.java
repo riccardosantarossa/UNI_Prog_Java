89f0c1d98a88cb53e13ae1b0b54a8196fccd80cb
riccardosantarossa <rikysanta03@gmail.com>
2023-05-20 20:15:53 +0200

    Random fatto, mancano gli shift sull'array

---
+++ Huffman/fileRandomico.java
@@ -0,0 +1,29 @@
+package Huffman;
+import huffman_toolkit.*;
+
+public class fileRandomico 
+{
+    public static void creaRandomFile(String nomeFile)
+    {
+        //InputTextFile in = new InputTextFile(nomeFile);
+        OutputTextFile out = new OutputTextFile(nomeFile);
+
+        int dimFile = 4096;
+
+        for (int i = 0; i < dimFile; i++) 
+        {
+            char c = (char) Math.abs(128*Math.random());
+            out.writeChar(c);
+        }
+
+        out.close();
+
+    }
+
+
+    public static void main(String[] args) 
+    {
+        creaRandomFile("out.txt");
+        huffman.comprimi("out.txt", "outCompresso.txt");   
+    }
+}
---
+++ Huffman/nodeQueue.java
@@ -0,0 +1,54 @@
+package Huffman;
+
+public class nodeQueue 
+{
+    //VARIABILI DI ISTANZA
+    private static nodo[] coda = new nodo[128];
+
+    //Costruttore
+    public nodeQueue()
+    {
+        coda = null;
+    }
+
+    //restituisce il numero di elementi contenuti nella coda
+    public static int size()
+    {
+        int numNodi = 0, index = 0;
+
+        while(coda[index] != null)
+        {
+            numNodi ++;
+            index ++;
+        }
+
+        return numNodi;
+
+    }
+
+    //restituisce l’elemento con “peso minore” (senza rimuoverlo dalla coda)
+    public static nodo peek()
+    {
+        return coda[0];
+    }
+
+    //restituisce e rimuove dalla coda l’elemento con “peso minore”
+    public static nodo poll()
+    {
+        nodo primo;
+
+        primo = coda[0];
+        coda[0] = null;
+
+        
+
+        return primo;
+
+    }
+
+    //aggiunge un nuovo elemento n alla coda
+    public static void add(nodo n)
+    {
+
+    }
+}
