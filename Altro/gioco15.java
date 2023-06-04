b73e0beb21bd2a344a566dc3fe472aca992412e1
riccardosantarossa <rikysanta03@gmail.com>
2023-06-04 14:53:41 +0200

    finalmente funziona

--- Altro/gioco15.java
+++ Altro/gioco15.java
@@ -1,5 +1,6 @@
 package Altro;
 import puzzleboard.*;
+import java.util.*;
 
 public class gioco15 
 {
@@ -10,38 +11,81 @@ public class gioco15
     public int posX= -1, posY=-1;
     public int pos0x=-1, pos0y=-1;
     
+    //Lista in cui vengono messi i numeri e mescolati prima di stampare il campo di gioco
+    private List<Integer> numbers = new ArrayList<>();
+    
 
     //Costruttore
     public gioco15(int n)
     {
-        int cont = -1;
+        
         campogGioco = new PuzzleBoard(n);
-
-        sottobosco = new  int[n][n];
+        
+        //Lista riempita randomicamente
+        random(n);
+    
+        sottobosco = new  int[n+1][n+1];
         dim = n;
-        for (int i = 0; i < n; i++) 
-        {
-            for (int j = 0; j < n; j++) 
-            {
-                cont++;
-                sottobosco[i][j] = cont;
-            }
+  
+        for (int i = 0; i < sottobosco.length; i++) {
+
+            for (int j = 0; j < sottobosco.length; j++) {
+
+                if(i==0 || j==0)
+                {
+                    //Colonna di -1 usata per ovviare alla partenza da 1 della matrice
+                    sottobosco[i][j]=-1;
+                }
+                else
+                {
+                    sottobosco[i][j] = numbers.get(0);
+                    numbers.remove(0);
+                }
+                
+            }            
         }
 
+        /*sottobosco[1][1]=1;
+        sottobosco[1][2]=2;
+        sottobosco[1][3]=3;
+        sottobosco[1][4]=4;
+        sottobosco[2][1]=5;
+        sottobosco[2][2]=6;
+        sottobosco[2][3]=7;
+        sottobosco[2][4]=8;
+        sottobosco[3][1]=9;
+        sottobosco[3][2]=10;
+        sottobosco[3][3]=11;
+        sottobosco[3][4]=12;
+        sottobosco[4][1]=13;
+        sottobosco[4][2]=14;
+        sottobosco[4][3]=0;
+        sottobosco[4][4]=15;*/
+
+        stampa(sottobosco);
         stampaCampoGioco();
 
     }
 
-    public static void stampa()
-    {
-        
-        for (int i = 0; i < dim; i++) 
-        {
-            for (int j = 0; j < dim; j++) 
-            {
-                System.out.println(sottobosco[i][j]); 
-            }
+    //Collezione di numeri random
+    public void random(int x) {
+
+        int cont =-1;
+        int d = x*x;
+
+        for (int i = 0; i < d; i++) {
+            cont++;
+            numbers.add(cont);
         }
+
+        Collections.shuffle(numbers);
+    }
+
+    //Stampa in console in forma matriciale
+    public static void stampa(int mat[][])
+    {
+        for (int[] row : mat)
+            System.out.println(Arrays.toString(row));
     }
 
     //Verifica se i tasselli sono ordinati
@@ -49,20 +93,24 @@ public class gioco15
     {
         boolean flag = true;
         int i, j;
-        i = dim-1;
-        j = dim-1;
-        int check = dim^2-1;
+        i = dim;
+        j = dim;
+        int check = (int) Math.pow(dim,2);
 
-        if(!(sottobosco[dim-1][dim-1]==0))
+        if(sottobosco[dim][dim]==0)
         {
-            while (flag && i >=0) 
+            while (flag && i >=1) 
             {
-                while(j>=0)
+                j=dim;
+                while(j>=1)
                 {
-                    if(i == dim-1 && j == dim-1)
+                    if(i == dim && j == dim)
+                    {
                         flag = true;
-               
-                    if(sottobosco[i][j]==check)
+                        check--;
+
+                    }
+                    else if(sottobosco[i][j]==check)
                     {
                         flag=true;
                         check--;
@@ -84,14 +132,15 @@ public class gioco15
         return flag; 
     }
 
+
     //Verificare spostamento
     public boolean controllaSpostamento()
     {
         int n = campogGioco.get();
         
-        for (int i = 0; i < dim; i++) 
+        for (int i = 1; i <= dim; i++) 
         {
-            for (int j = 0; j < dim; j++) 
+            for (int j = 1; j <= dim; j++) 
             {
                 if(sottobosco[i][j] == n)
                 {
@@ -101,25 +150,25 @@ public class gioco15
             }
         }
 
-        if( posX > 0 && sottobosco[posY][posX-1] == 0)
+        if( (posX > 1 && posX<=dim) && sottobosco[posY][posX-1] == 0)
         {
             pos0x = posX-1;
             pos0y = posY;
             return true;
         }
-        else if( posY > 0 && sottobosco[posY-1][posX] == 0  )  
+        else if( (posY > 1 && posY<=dim) && sottobosco[posY-1][posX] == 0  )  
         {
             pos0x = posX;
             pos0y = posY-1;
             return true;
         }
-        else if( posY < dim-1 && sottobosco[posY][posX+1] == 0)
+        else if( (posX>=1 && posX < dim) && sottobosco[posY][posX+1] == 0)
         {
             pos0x = posX+1;
             pos0y = posY;
             return true;
         }
-        else if( posX < dim-1 && sottobosco[posY+1][posX] == 0)
+        else if( (posY>=1 && posY < dim) && sottobosco[posY+1][posX] == 0)
         {
             pos0x = posX;
             pos0y = posY+1;
@@ -129,12 +178,6 @@ public class gioco15
             return false;
     }
 
-    /* 
-    //Genera la soluzione testuale
-    public String configTestuale()
-    {
-
-    }*/
 
     //Spostare tassello
     public void spostaNumero()
@@ -148,18 +191,17 @@ public class gioco15
         }
     }
 
+    //Stampa in forma grafica il campo di gioco
     public void stampaCampoGioco()
     {
-        for (int i = 0; i < dim; i++) 
+        for (int i = 1; i <= dim; i++) 
         {
-
-            for (int j = 0; j < dim; j++) 
+            for (int j = 1; j <= dim; j++) 
             {
                 int x = sottobosco[i][j];
-                campogGioco.setNumber(i+1, j+1, x);
+                campogGioco.setNumber(i, j, x);
                 campogGioco.display();
             }
-            
         }
 
         if(haiVinto())
@@ -175,6 +217,11 @@ public class gioco15
          while (!a.haiVinto()) 
          {
             a.spostaNumero();
+
+            //Stampa anche la configurazione sotto forma di matrice
+            System.out.println("-------------------------------");
+            stampa(sottobosco);
+            System.out.println("-------------------------------");
          }
     }
 
