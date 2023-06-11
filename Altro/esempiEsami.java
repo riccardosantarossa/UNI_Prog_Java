48e3aed245eaa318cd275e23e8706d9355b67d88
riccardosantarossa <rikysanta03@gmail.com>
2023-06-11 17:32:30 +0200

    aggiunto un altro esercizio

--- Altro/esempiEsami.java
+++ Altro/esempiEsami.java
@@ -361,7 +361,55 @@ public class esempiEsami
     }
 
 
+    //ESAME 06/2019 es.3
+    public static long st( int n, int k ) 
+    { // n, k > 0
+        long[] ct = new long[] { 0 }; // contatore: variabile di stato
+        sRec( n, k, 1, ct );
+        return ct[0];
+    }
+
+    private static void sRec( int n, int k, int q, long[] ct ) 
+    {
+        if ( (k == 1) || (k == n) ) 
+        {
+            ct[0] = ct[0] + q;
+        } else 
+        {
+            sRec( n-1, k-1, q, ct );
+            sRec( n-1, k, k*q, ct );
+        }
+    }
+   
+    public static long stIter( int n, int k ) // n, k > 0
+    { 
+        long[] ct = new long[] { 0 };
+        Stack<int[]> stack = new Stack<int[]>();
+        
+        int[] f = new int[] {n, k, 1};
+        stack.push( f );
+
+        while ( !stack.empty()) 
+        {
+            f = stack.pop();
+
+            if ( (f[1] == 1) || ( f[1] == f[0]) ) 
+            {
+                ct[0] = ct[0] + f[2];
+
+            } else 
+            {
+                stack.push( new int[] { f[0] - 1, f[1] - 1, f[2]} );
+                stack.push( new int[] { f[0] - 1, f[1], f[1] * f[2]} );
+            }
+        }
+            return ct[0];
+    }
+
     
+    //ESAME 06/2017 es.4
+    
+
 
     //-------------------------------------------------------------------------------------------------//
     // FILE ESEMPI D'ESAME
@@ -658,10 +706,9 @@ public class esempiEsami
             System.out.print(matrSimm(new int[][]{{1,0,3}, {2,4,6}, {7,0,1}}));
             System.out.println(f(7,8));
             System.out.println(g(7,8));
-        */
-
             System.out.println(fuckIt(3,2,1));
             System.out.println(fuckItDP(3,2,1));
-            
+        */
+            System.out.print(stIter(6, 3));
     }
 }
